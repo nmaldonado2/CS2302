4298904909a13415d4cd33b1bf305f502f9b4cb3
nmaldonado2 <54590541+nmaldonado2@users.noreply.github.com>
2019-08-30 17:32:31 -0600

    Add files via upload

--- LAB1/Part2/NicholeMaldonado_Lab1_Part2.py
+++ LAB1/Part2/NicholeMaldonado_Lab1_Part2.py
@@ -1,88 +1,211 @@
 # CS2302 Data Structures
 # Nichole Maldonado
-# Lab 1 - Recursion, Part 1
+# Lab 1 - Recursion, Part 2
 # Dr. Fuentes
 # Anindita Nath
 
 import time
 import os
 
-
-# 
-# Inputs:
-# Outputs:
-def ReadAndStoreTextFromFile(fileName):
-    try:
-        # Opens file to be read and evaluated
-        file = open(fileName, "r")
-        
-        englishWordSet = set()
-        
-        line = file.readline()
-        while line:
-            # Adds each word to englishWordSet without the appended endline
+# Opens the file containing all english words and populates englishWordSet
+# with these words.
+# Input: The path of the file containing the english words and sets for the
+#        english words and prefixes that will be populated.
+# Output: None
+# Assume that englishWordSet and prefixWordSet are initially empty sets.
+def ReadAndStoreTextFromFile(filePath, englishWordSet, prefixWordSet):
+    # Opens file to be read and evaluated
+    file = open(filePath, "r")
+    line = file.readline()
+    
+    # Iterates through each line of the file adding all non-white space words
+    # to englishWordSet and prefixWordSet.
+    while line:
+        if not line.isspace():
             englishWordSet.add(line.rstrip("\n"))
-            line = file.readline()
-    except FileNotFoundError: 
-        print("The words_alpha file could not be found")
-        return None
-    except IOError:
-        print("The words_alpha file could not be accessed")
-        return None
-    
+            AddPrefixes(line.rstrip("\n"), prefixWordSet)
+        line = file.readline()
     file.close()
-    return englishWordSet
 
-def FindAnagrams(remaining_word, appending_word, englishWordSet, anagrams, original_word):
-    if len(remaining_word) == 0:
-        if appending_word in englishWordSet:
-            anagrams.append(appending_word)
-        return
-    # FIXME: Should I prevent original word from being added initially or delete
-    # it at the end
-    if len(remaining_word) == 1 and (appending_word + remaining_word) == original_word:
-        return
+# Function that adds all possible prefixes of word that do not already exist in
+# prefixWordSet.
+# Input: word whose prefixes will be extracted and the set, prefixWordSet.
+# Output: None
+def AddPrefixes(word, prefixWordSet):
+    if not word in prefixWordSet:
+        prefix = word[:len(word) - 1]
         
+        # Adds prefixes by removing the last letter of the word until the 
+        # prefix exists in the set or ultimately an empty string is reached.
+        while not prefix in prefixWordSet:
+            prefixWordSet.add(prefix)
+            prefix = prefix[:len(prefix) - 1]
+
+# Recursive function that finds all anagrams of originalWord
+# Input: remainingLetters which contains the left over letters to be added to 
+#        appendingWord. Sets englishWordSet and prefixWordSet contain all the
+#        all english words and prefixes respectively. anagrams is a set that
+#        will contain all the anagrams found.
+# Output: None, however anagrams may be populated if anagrams of originalWord
+#         are found.
+def FindAnagrams(remainingLetters, appendedLetters, englishWordSet, 
+                 prefixWordSet, anagrams, originalWord):
+    
+    # Base case that adds appended letters to anagrams if it is an anagram and
+    # there are no more remaining letters.
+    if len(remainingLetters) == 0:
+        if appendedLetters in englishWordSet:
+            anagrams.append(appendedLetters)
+        return
+    
+    # Base case that stops recursive calls if the next call will create 
+    # originalWord
+    if len(remainingLetters) == 1 and (appendedLetters + remainingLetters) == originalWord:
+        return
+    
+    # If the appendedLetters is not in prefixWordSet, then the recursive branch
+    # is stopped.
+    if not appendedLetters in prefixWordSet:
+        return
+    
     charUsedSet = set()
-    for i in range(len(remaining_word)):
-        charUsedSet.add(remaining_word[i])
-        newLetterToAppend = remaining_word[i]
-        newRemainingWord = remaining_word[:i] + remaining_word[i + 1:]
-        FindAnagrams(newRemainingWord, appending_word + newLetterToAppend, englishWordSet, anagrams, original_word)
     
+    # Makes recursive calls for every unique letter in remainingLetter by 
+    # removing the letter from remainingLetter and appending it to 
+    # appendedLetters.
+    for i in range(len(remainingLetters)):
+        if not remainingLetters[i] in charUsedSet:
+            
+            # Since the current letter will be appended, it is added to the set
+            # to prevent future duplicates.
+            charUsedSet.add(remainingLetters[i])
+            newLetterToAppend = remainingLetters[i]
+            newRemainingWord = remainingLetters[:i] + remainingLetters[i + 1:]
+            FindAnagrams(newRemainingWord, appendedLetters + newLetterToAppend,
+                         englishWordSet, prefixWordSet, anagrams, originalWord)
+
+
+# Ensures that fileName is a .txt file.
+# Input: a string of the file's name that will be evaluated.
+# Output: Returns false if the fileName does not end in .txt and returns true
+#         true otherwise.
+def CheckIfTxtFile(fileName):
     
-#response = requests.get("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt")
-#if response:
+    # Returns false if the fileName does not have at least letters since a
+    # valid .txt file name can have a minimum of 5 letters.
+    if len(fileName) < 5:
+        print("Invalid .txt file. Please try again.")
+        return False
+    
+    # If the length of fileName is greater than or equal to 5, then true is
+    # returned only if fileName ends in ".txt".
+    else:
+        if fileName[-4:] == ".txt" and not "." in fileName[:-4]:
+            return True
+        else:
+            print("Invalid .txt file. Please try again.")
+            return False
+        
+# Allows the user to modify the file directory and name for the file containing
+# all the english words, in the event the file cannot be found.
+# Input: A list, fileAttributes, which contains [directory, filename]
+# Output: None, however fileAttributes will contain a new file name or 
+#         directory based on the user's choice.
+# Assume fileAttributes only has the directory and file name as elements.
+def FilePathSetup(fileAttributes):
+    # menuNum is initialized to 0 to initially enter the while loop.
+    menuNum = 0
 
-#fileName = os.path.join(os.getcwd(), "words_alph.txt")
-fileName = "words_alpha.txt"
-englishWordSet = ReadAndStoreTextFromFile(fileName)
+    print("Current file name:",fileAttributes[0])
+    print("Current directory:", fileAttributes[1],"\n")
+    
+    # Allows the user to edit the directory or file name for the english word
+    # file.
+    while menuNum != 1 and menuNum != 2:
+        print("1. Change directory")
+        print("2. Change file name")
+        try:
+            menuNum = int(input("Select 1 or 2: "))
+            
+            # Changes the directory name.
+            if menuNum == 1:
+                fileAttributes[0] = input("File directory (Do not include file name): ")
+            
+            # Changes the file name.
+            elif menuNum == 2:
+                fileAttributes[1] = input("File Name (do not include directory): ")
+                
+                # Ensures that the new file name is a .txt file.
+                while not CheckIfTxtFile(fileAttributes[1]):
+                    fileAttributes[0] = input("File Name (do not include directory):")
+            
+            # Notifies the user if they entered a value other than 1 or 2.
+            else:
+                print("\nInvalid menu number. Please try again.")
+        
+        # Notifies the user if they entered a menu velue other than 1 or 2. 
+        except ValueError:
+            print("\nInvalid menu selection. Please try again.")
+    
+# FIXME: Would an empty file still have a prefixWordSet with just "" ?
+# FIXME: Should the set be alphabatizeds. I looked and sets are usually "unsorted"
+            
+       
+englishWordSet = set()
+prefixWordSet = set()
+fileAttributes = [os.getcwd(), "words_alpha.txt"]
+# fileRead assigned to false since the file has not been read yet.
+fileRead = False
 
-if englishWordSet is None:
-    print("Please make sure the words_alpha.txt is in the current program directory")
-    print("Program terminating")
+# Repeatedly attempts to open the file containing all the english words until
+# the correct file is found.
+while not fileRead:
+    try:
+        # Calls ReadStoreTextFromFile to open the file and fill the 
+        # set arguements accordingly.
+        ReadAndStoreTextFromFile(os.path.join(fileAttributes[0], 
+                     fileAttributes[1]), englishWordSet, prefixWordSet)
+        
+        # fileRead assigned to true since the ReadAndStoreTextFromFile function
+        # did not through an error.
+        fileRead = True
+        
+    # Catches a FileNotFoundError or IOError and calls FilePathSetup to ensure
+    # that the file path is correct.
+    except FileNotFoundError:
+        print("\nFile not found. Please edit the file path.\n")
+        FilePathSetup(fileAttributes)
+    except IOError:
+        print("\nFile could not be accessed. Please edit the file path.\n")
+        fileAttributes = FilePathSetup(fileAttributes)
 
-else:
+wordOrEmptyString = input("Enter a word or empty string to finish: ").lower()
+
+# Finds and prints the anagrams of the user's choice as long as 
+# wordOrEmptyString is not empty.
+while len(wordOrEmptyString) > 0:
+    
+    # Finds anagrams as long as wordOrEmpty string has letter characters only.
+    if wordOrEmptyString.isalpha():
+        anagrams = []
+        
+        # Calculates time for the FindAnagrams recursive method.
+        startTime = time.perf_counter()
+        FindAnagrams(wordOrEmptyString, "", englishWordSet, prefixWordSet, 
+                     anagrams, wordOrEmptyString)
+        elapsedTime = time.perf_counter() - startTime
+        
+        # Prints the anagrams in alphabetical order and reports the 
+        # performance time.
+        print("The word", wordOrEmptyString, "has the following", len(anagrams), " anagrams: ")
+        anagrams.sort()
+        for i in anagrams:
+           print(i)
+        print("It took %.6f seconds to find the anagrams\n" % elapsedTime)
+    else:
+        print("The word you entered is incorret. Please make sure that the word contains letters only and no spaces\n")
+    
+    # Allows the user to enter new word or empty string to quit.
     wordOrEmptyString = input("Enter a word or empty string to finish: ").lower()
     
-    while len(wordOrEmptyString) > 0:
-        
-        # Finds anagrams as long as wordOrEmpty string has letter characters only.
-        if wordOrEmptyString.isalpha():
-            anagrams = []
-            startTime = time.perf_counter()
-            FindAnagrams(wordOrEmptyString, "", englishWordSet, anagrams, wordOrEmptyString)
-            elapsedTime = time.perf_counter() - startTime
-            print("The word", wordOrEmptyString, "has the following", len(anagrams), " anagrams: ")
-            anagrams = list(set(anagrams))
-            for i in anagrams:
-               print(i)
-            print("It took %.6f seconds to find the anagrams\n" % elapsedTime)
-        else:
-            #is this statment ok if there are white spaces only?
-            print("The word you entered is incorret. Please make sure that the word contains letters only and no spaces\n")
-        wordOrEmptyString = input("Enter a word or empty string to finish: ").lower()
-        
-    print("Bye, thanks for using this program!")
-#else:
-#    print("An error occured while loading the file. Program terminating")
+print("Bye, thanks for using this program!")
